334fc9d185cc8d228f19a366a0270b0b3172d785
Adli Husain <adlihusain716@gmail.com>
2019-11-04 07:54:45 +1300

    Second commit

---
+++ build/precache-manifest.9763821cd98335ff480de3cf4ca3bf2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2da3718fc49f15f1dea31489be22765b",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "0c1a59f540849411a0a0",
+    "url": "/pokedex/static/css/main.3f21249b.chunk.css"
+  },
+  {
+    "revision": "70426624dd05dd31b92f",
+    "url": "/pokedex/static/js/2.87113c2c.chunk.js"
+  },
+  {
+    "revision": "0c1a59f540849411a0a0",
+    "url": "/pokedex/static/js/main.9a11af57.chunk.js"
+  },
+  {
+    "revision": "c130da2e2f62582e6f11",
+    "url": "/pokedex/static/js/runtime-main.8072c1f3.js"
+  }
+]);
