c1e6a1a34b4184bc34f84cf0c82ebb10d3ad9c9d
Adli Husain <adlihusain716@gmail.com>
2019-11-04 08:04:50 +1300

    Updates

---
+++ precache-manifest.ab98fb4411a6ac7540237fde595a8c3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f83b04b520d4d44b44b2f7b4af7a1e",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "9195b9b41a76c8b7c29d",
+    "url": "/pokedex/static/css/main.3f21249b.chunk.css"
+  },
+  {
+    "revision": "70426624dd05dd31b92f",
+    "url": "/pokedex/static/js/2.87113c2c.chunk.js"
+  },
+  {
+    "revision": "9195b9b41a76c8b7c29d",
+    "url": "/pokedex/static/js/main.24ad6e41.chunk.js"
+  },
+  {
+    "revision": "c130da2e2f62582e6f11",
+    "url": "/pokedex/static/js/runtime-main.8072c1f3.js"
+  }
+]);
